68899914276803e3c05fb3e28691c6e752ff750f
Dhrubo <dhrubovi@gmail.com>
2020-10-01 15:35:58 +0530

    Migratory Birds

---
+++ src/implementation/easy/MigratoryBirds.java
@@ -0,0 +1,29 @@
+package implementation.easy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MigratoryBirds {
+
+    public static void main(String[] args) {
+        Integer[] arr = {1,4,4,4,5,3};
+        System.out.println(migratoryBirds(Arrays.asList(arr)));
+    }
+
+    static int migratoryBirds(List<Integer> arr){
+        if(arr.size() == 0 ) return 0;
+        int[] types = new int[5];
+        for(int i : arr){
+            ++types[i-1];
+        }
+        int max = types[0];
+        int type = 1;
+        for(int i =0; i < 5 ; i++){
+            if(types[i] > max){
+                max = types[i];
+                type = i + 1;
+            }
+        }
+        return type;
+    }
+}
