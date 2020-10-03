cc56a1f86f137903b2b776fc30c6bfe9e63dba65
Dhrubo <7534178+DhruboB@users.noreply.github.com>
2020-10-03 19:42:25 +0530

    Binary Search Tree : Lowest Common Ancestor

---
+++ src/bst/BSTLowestCommonAncestor.java
@@ -0,0 +1,71 @@
+package bst;
+
+public class BSTLowestCommonAncestor {
+
+    /*
+    class Node
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+    public static Node lca(Node root, int v1, int v2) {
+        if(root == null) return null;
+
+        if(root.data == v1 || root.data == v2)
+            return root;
+
+        Node leftLCA = lca(root.left, v1, v2);
+        Node rightLCA = lca(root.right, v1, v2);
+
+        if(leftLCA != null && rightLCA != null )
+            return root;
+
+        return leftLCA != null ? leftLCA : rightLCA;
+    }
+
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        int t = 6;
+        int[] arr = {4, 2, 3, 1, 7, 6};
+        Node root = null;
+        while (t-- > 0) {
+            int data = arr[t];
+            root = insert(root, data);
+        }
+        int v1 = 1;
+        int v2 = 7;
+        Node ans = lca(root, v1, v2);
+        System.out.println(ans.data);
+    }
+
+
+}
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
