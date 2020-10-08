675c3e25fbc5ef0474d8e7dba6c72de86dd64326
Dhrubo <7534178+DhruboB@users.noreply.github.com>
2020-10-08 17:59:07 +0530

    New Year Chaos - try 2

--- src/interview/arrays/NewYearChaos.java
+++ src/interview/arrays/NewYearChaos.java
@@ -11,44 +11,30 @@ public class NewYearChaos {
         minimumBribes(arr);
     }
 
+    // Complete the minimumBribes function below.
     static void minimumBribes(int[] arr) {
         int minBribes = 0;
-        TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > i + 3 || arr[i] < i - 3) {
+        int expectedFirst = 1;
+        int expectedSecond = 2;
+        int expectedThird = 3;
+
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] == expectedFirst) {
+                expectedFirst = expectedSecond;
+                expectedSecond = expectedThird;
+                ++expectedThird;
+            } else if (arr[i] == expectedSecond) {
+                ++minBribes;
+                expectedSecond = expectedThird;
+                ++expectedThird;
+            } else if (arr[i] == expectedThird) {
+                minBribes += 2;
+                ++expectedThird;
+            } else {
                 System.out.println("Too chaotic");
                 return;
             }
-            map.put(arr[i], i+1);
-        }
-
-        for(Map.Entry key : map.entrySet()){
-            int val = (Integer)key.getKey();
-            int pos = (Integer)key.getValue();
-            if (val == pos) {
-                continue;
-            }
-            System.out.println(map.toString());
-            for(int i = pos ; i < val  ; i++ ) {
-                swapPosition(map, arr, val);
-                minBribes++;
-            }
         }
         System.out.println(minBribes);
-    }
-
-    static void swapPosition(TreeMap<Integer,Integer> map , int[]  arr, int val1){
-        Integer pos1 = map.get(val1); // 8th pos
-        Integer pos2 = pos1 + 1; // target position
-        Integer val2 = arr[pos2-1]; // target value
-        map.put(val1, pos2);
-        map.put(val2,pos1);
-        swap(arr, pos1-1,pos2-1);
-    }
-
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
