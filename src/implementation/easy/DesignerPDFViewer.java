1121ae791a297b4e006755c8c49c47eea65a7ee7
Dhrubo <7534178+DhruboB@users.noreply.github.com>
2020-10-12 19:02:01 +0530

    Designer PDF Viewer

---
+++ src/implementation/easy/DesignerPDFViewer.java
@@ -0,0 +1,24 @@
+package implementation.easy;
+
+public class DesignerPDFViewer {
+
+    public static void main(String[] args) {
+        int[] heights = {1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7};
+        String word = "zaba";
+        System.out.println(designerPdfViewer(heights,word));
+    }
+
+    // Complete the designerPdfViewer function below.
+    static int designerPdfViewer(int[] heights, String word) {
+        int maxHeight = Integer.MIN_VALUE ;
+        int len = word.length();
+        for(int i = 0; i < len ; i++){
+            int chr = word.charAt(i);
+            int height = heights[chr - 'a'] ;
+            if(maxHeight < height){
+                maxHeight = height;
+            }
+        }
+        return maxHeight * len;
+    }
+}
