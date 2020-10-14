107656c72ec90c955c709081225e8ba288f35bf2
Dhrubo <7534178+DhruboB@users.noreply.github.com>
2020-10-14 17:30:20 +0530

    Sequence Equation

---
+++ src/implementation/easy/CutTheSticks.java
@@ -0,0 +1,33 @@
+package implementation.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CutTheSticks {
+
+    public static void main(String args[]) {
+        int[]  arr = {1,2,3,4,3,3,2,1};
+//        int[]  arr = {5,4,4,2,2,8};
+//        int[]  arr = {1,1,1,1};
+        System.out.println(Arrays.toString(cutTheSticks(arr)));
+    }
+
+    // Complete the cutTheSticks function below.
+    static int[] cutTheSticks(int[] arr) {
+        List<Integer> list  = new ArrayList<>();
+        Arrays.sort(arr);
+        int len = arr.length;
+        list.add(len);
+        for(int i = 1 ; i < len ; i++){
+            int j = i;
+            while(j < len && arr[j] == arr[j-1]){
+                j++;
+                i++;
+            }
+            if(len -i != 0)
+                list.add(len - i);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
---
+++ src/implementation/easy/SequenceEquation.java
@@ -0,0 +1,29 @@
+package implementation.easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SequenceEquation {
+    public static void main(String[] args) {
+        int[] p = {2,3,1};
+        for(Integer val: permutationEquation(p)) {
+            System.out.println(val);
+        }
+    }
+
+    // Complete the permutationEquation function below.
+    static List<Integer> permutationEquation(int[] p) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int len = p.length;
+        List<Integer> list = new ArrayList<>();
+        for(int i=0 ; i < len; i++){
+            map.put(p[i], i+1);
+        }
+        for(int i=0 ; i < len ;i++){
+            list.add(map.get(map.get(i+1)));
+        }
+        return list;
+    }
+}
