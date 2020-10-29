318bf8d378bba0489e762fb0328f6fdfa04d15e0
Dhrubo <7534178+DhruboB@users.noreply.github.com>
2020-10-29 19:25:23 +0530

    Queens Attack II

--- src/implementation/QueensAttackII.java
+++ src/implementation/QueensAttackII.java
@@ -10,99 +10,64 @@ public class QueensAttackII {
 //        int[][] obstacles = null; // 9
 
 
-//        int n = 5;
-//        int k = 3;
-//        int r_q = 4;
-//        int c_q = 3;
+        int n = 5;
+        int k = 3;
+        int r_q = 4;
+        int c_q = 3;
+        int[][] obstacles = new int[][]{
+                {5, 5},
+                {4, 2},
+                {2, 3}
+        };
+
+//        int n = 100000;
+//        int k = 0;
+//        int r_q = 4187;
+//        int c_q = 5068;
 //        int[][] obstacles = null;
 
-        int n = 100000;
-        int k = 0;
-        int r_q = 4187;
-        int c_q = 5068;
-        int[][] obstacles = null;
-
-        if (k != 0) {
-            obstacles = new int[][]{
-                                    {5, 5},
-                                    {4, 2},
-                                    {2, 3}
-                                    };
-        }
-            System.out.println(queensAttack(n, k, r_q, c_q, obstacles));
-        }
-
-        // Complete the queensAttack function below.
-        static int queensAttack ( int n, int k, int r_q, int c_q, int[][] obstacles){
-            int count = 0;
-            boolean[][] isObstaclePresent = new boolean[n][n];
-            initialize(isObstaclePresent, obstacles);
-            int x = r_q - 1;
-            int y = c_q - 1;
-            for (int i = x + 1; i < n; i++) {
-                if (isObstaclePresent[i][y]) {
-                    break;
-                }
-                count++;
-            }
-            for (int i = x - 1; i >= 0; i--) {
-                if (isObstaclePresent[i][y]) {
-                    break;
-                }
-                count++;
-            }
-            for (int i = y + 1; i < n; i++) {
-                if (isObstaclePresent[x][i]) {
-                    break;
-                }
-                count++;
-            }
-            for (int i = y - 1; i >= 0; i--) {
-                if (isObstaclePresent[x][i]) {
-                    break;
-                }
-                count++;
-            }
-            outer1:
-            for (int i = x + 1, j = y + 1; i < n && j < n; i++, j++) {
-                if (isObstaclePresent[i][j]) {
-                    break outer1;
-                }
-                count++;
-            }
-
-            outer2:
-            for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
-                if (isObstaclePresent[i][j]) {
-                    break outer2;
-                }
-                count++;
-            }
-
-            outer3:
-            for (int i = x + 1, j = y - 1; i < n && j >= 0; i++, j--) {
-                if (isObstaclePresent[i][j]) {
-                    break outer3;
-                }
-                count++;
-            }
-
-            outer4:
-            for (int i = x - 1, j = y + 1; i >= 0 && j < n; i--, j++) {
-                if (isObstaclePresent[i][j]) {
-                    break outer4;
-                }
-                count++;
-            }
-            return count;
-        }
-
-        static void initialize ( boolean[][] isObstaclePresent, int[][] obstacles){
-            if (obstacles == null || obstacles.length == 0) return;
-            for (int i = 0; i < obstacles.length; i++) {
-                for (int j = 0; j < 2; j++) {
-                    isObstaclePresent[obstacles[i][0]-1][obstacles[i][1]-1] = true;
-                }
-            }
-        }
+        System.out.println(queensAttack(n, k, r_q, c_q, obstacles));
     }
+
+    // Complete the queensAttack function below.
+    static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
+        int up = n - r_q;
+        int down = r_q - 1;
+        int right = n - c_q;
+        int left = c_q - 1;
+
+        int ruMin = Math.min(up, right);
+        int rdMin = Math.min(right, down);
+        int luMin = Math.min(left, up);
+        int ldMin = Math.min(left, down);
+
+        if (obstacles == null) return up + down + right + left + ruMin + rdMin + luMin + ldMin;
+
+        for (int[] oXY : obstacles) {
+            if (oXY[1] == c_q) {
+                if (oXY[0] < r_q)
+                    down = Math.min(down, r_q - 1 - oXY[0]);
+                else
+                    up = Math.min(up, oXY[0] - r_q - 1);
+            } else if (oXY[0] == r_q) {
+                if (oXY[1] < c_q)
+                    left = Math.min(left, c_q - 1 - oXY[1]);
+                else
+                    right = Math.min(right, oXY[1] - c_q - 1);
+            } else if (Math.abs(oXY[0] - r_q) == Math.abs(oXY[1] - c_q)) {
+                if (oXY[1] > c_q) {
+                    if (oXY[0] > r_q)
+                        ruMin = Math.min(ruMin, oXY[1] - c_q - 1);
+                    else
+                        rdMin = Math.min(rdMin, oXY[1] - c_q - 1);
+                } else {
+                    if (oXY[0] > r_q)
+                        luMin = Math.min(luMin, c_q - 1 - oXY[1]);
+                    else
+                        ldMin = Math.min(ldMin, c_q - 1 - oXY[1]);
+                }
+            }
+        }
+        return up + down + right + left + ruMin + rdMin + luMin + ldMin;
+    }
+}
